9a2f5218321ba2b36a39687024514ee3e9aaea0b
niwanowa <nwianowa@gmail.com>
2023-10-04 16:54:34 +0900

    issueリストを毎回リセットするように

--- .github/workflows/update_readme/update_readme.py
+++ .github/workflows/update_readme/update_readme.py
@@ -2,10 +2,11 @@ import os
 import json
 
 # 環境変数をからREPOSITORYを取得
-repository = os.environ['REPOSITORY']
+repository = os.environ.get('REPOSITORY')
 
 # repositoryから、githubAPIを用いてIssueのリストを取得
 issues = json.loads(os.popen(f'curl -s https://api.github.com/repos/{repository}/issues').read())
+print(issues)
 for issue in issues:
     print("----")
     print(issue['title'])
@@ -14,14 +15,18 @@ for issue in issues:
 # README.mdの読み込み
 with open('../../../README.md', 'r') as f:
     readme_content = f.read()
+    print(readme_content)
 
-# # Issueリストの生成
+# Issueリストの生成
 issue_list = [f"- [{issue['title']}]({issue['html_url']})" for issue in issues]
+print('----------------')
+print(issue_list)
 
-# # README.mdの更新
-readme_content = readme_content.replace('<!-- ISSUE_LIST_START -->\n<!-- This section will be automatically updated by the GitHub Action -->\n<!-- ISSUE_LIST_END -->', '<!-- ISSUE_LIST_START -->\n{}\n<!-- ISSUE_LIST_END -->'.format('\n'.join(issue_list)))
+# README.mdの更新
+# <!-- ISSUE_LIST_START --> と <!-- ISSUE_LIST_END --> の間を削除して、新しいIssueリストを挿入
+readme_content = readme_content.split('<!-- ISSUE_LIST_START -->')[0] + '<!-- ISSUE_LIST_START -->\n' + '\n'.join(issue_list) + '\n<!-- ISSUE_LIST_END -->' + readme_content.split('<!-- ISSUE_LIST_END -->')[1]
 
-# # 更新内容を書き込み
+# 更新内容を書き込み
 with open('../../../README.md', 'w') as f:
     f.write(readme_content)
 
